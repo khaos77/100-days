5ebd3b09c3ad771749ca7759d63a0dc4ab83771b
khaos77 <contato.isaa0l@gmail.com>
2024-02-05 23:30:53 -0300

    faltei dois dias e quase era 3 mas tem q voltar pra fechar 100

---
+++ day22/kaggle.py
@@ -0,0 +1,41 @@
+#1
+def is_negative(number):
+    if number < 0:
+        return True
+    else:
+        return False
+
+def concise_is_negative(number):
+    pass # Your code goes here (try to keep it to one line!)
+    return True if number < 0 else False
+
+# Check your answer
+q4.check()
+
+#2
+def prepared_for_weather(have_umbrella, rain_level, have_hood, is_workday):
+    # Don't change this code. Our goal is just to find the bug, not fix it!
+    return have_umbrella or rain_level < 5 and have_hood or not rain_level > 0 and is_workday
+
+# Change the values of these inputs so they represent a case where prepared_for_weather
+# returns the wrong answer.
+have_umbrella = False
+rain_level = 0.0
+have_hood = False
+is_workday = False
+
+# Check what the function returns given the current values of the variables above
+actual = prepared_for_weather(have_umbrella, rain_level, have_hood, is_workday)
+print(actual)
+
+# Check your answer
+q3.check()
+
+#3
+def wants_all_toppings(ketchup, mustard, onion):
+    """Return whether the customer wants "the works" (all 3 toppings)
+    """
+    return ketchup and mustard and onion 
+
+# Check your answer
+q5.a.check()
