da5b050b59d0ba118fbbdb6ef2131669d40d13ea
khaos77 <contato.isaa0l@gmail.com>
2024-02-16 23:14:50 -0300

    dia 27 simples

---
+++ day27/acidez.py
@@ -0,0 +1,11 @@
+n = float(input())
+while n != -1:
+    if n < 7:
+        print("ACIDA")
+    else:
+        if n == 7:
+            print("NEUTRA")
+        else:
+            print("BASICA")
+    n = float(input())
+        
