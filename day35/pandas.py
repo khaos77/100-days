0901feb52d1851ac63a3f1fc06f24eb668efa24e
khaos77 <contato.isaa0l@gmail.com>
2024-02-29 22:43:08 -0300

    disciplina eh importante dia 35

---
+++ day35/pandas.py
@@ -0,0 +1,20 @@
+#cria series
+pd.myseries.Series
+
+#primeiro termo da series
+myseries[0]
+
+#Series only works with a list of things, so its labels are index
+myseriea = (mysereis, index='x','y','z')
+
+#cria dataframe
+pd.DataFrame(data)
+
+#first row dataframe
+pd.loc[0]
+
+#load csv file
+df.read_csv().data
+
+#return all dataframe
+df.to_string()
