0d33e2b03423fbb213868870ae1825e7557af7e7
khaos77 <contato.isaa0l@gmail.com>
2024-03-09 23:34:56 -0300

    so Deus sabe como ta a mente do palhaço dia 36

---
+++ day36/4549administradora_cartao.py
@@ -0,0 +1,7 @@
+a = float(input())
+b = a / 2
+c = (b * 0.065) + b
+
+print('Valor total da fatura: R$ %.2f'% (a))
+print('Valor a pagar em Marco: R$ %.2f'% (b))
+print('Valor a pagar em Abril: R$ %.2f'%(c))
