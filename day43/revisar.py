c12a9ba39c3c97626713e80b09b93dc84c4d5092
khaos77 <contato.isaa0l@gmail.com>
2024-03-25 23:07:50 -0300

    revisao dia 43

--- day43/revisar.py
+++ day43/revisar.py
@@ -7,3 +7,16 @@ import pandas as pd
 data = ['9', '6', '23', '3', '3']
 df = pd.DataFrame(data)
 print(df.value_counts())
+#ascending controla a ordem com qual aparece o resultado, o normalize resulta o valor da frequencia relativa
+
+plt.axvline(x=5, color='red', linestyle='--')
+#aparece no grafico uma linha pontilhada
+
+df = df.dropna(subset=['2022-23'])
+#dropna retira valores vazios da coluna
+
+sns.boxplot()
+#boxplot so vai com import sns
+
+.flatten()
+#a multi-dimensional array, flatten() will convert it into a one-dimensional array 
